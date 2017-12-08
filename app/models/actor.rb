5164d0d0c16ffe5922ac3fb0aa2ef9157ef5fad6
Lara Sanders <larasanders@gmail.com>
2017-12-08 17:35:22 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,8 +4,7 @@ class Actor < ActiveRecord::Base
 
   def full_name
     actor = Actor.create(first_name: first_name, last_name: last_name)
-    actor.first_name
-    actor.last_name
+    actor.first_name actor.last_name
 end
 
 def list_roles
